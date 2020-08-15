1f4ad44c9b2eb7678a964661c2af9314c92bd122
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-08-15 10:08:06 +0300

    Finalized the User Authentication - sign-up, login, logout

--- Todo_Project/action/views.py
+++ Todo_Project/action/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+
+def home(request):
+    return render(request, 'action/home.html')
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'action/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'action/loginuser.html', {'form': AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 
 def signupuser(request):
@@ -20,6 +36,13 @@ def signupuser(request):
                 return render(request, 'action/signupuser.html', {'form':UserCreationForm(), 'error':'Username already taken, please use a different username'})
         else:
             return render(request, 'action/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
 
 def currenttodos(request):
     return render(request, 'action/currenttodos.html')
